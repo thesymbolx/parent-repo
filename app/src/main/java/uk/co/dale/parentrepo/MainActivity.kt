02e99e9daa6fedafa8a8f6e6a4728121d54d7d8a
Dale Evans <dale.evans1984@gmail.com>
2025-06-14 09:29:06 +0100

    Remove android tests

--- app/src/main/java/uk/co/dale/parentrepo/MainActivity.kt
+++ app/src/main/java/uk/co/dale/parentrepo/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import uk.co.dale.design_catalog.DaleSuperTextBox
 import uk.co.dale.parentrepo.ui.theme.ParentRepoTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +21,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             ParentRepoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    innerPadding
+                    DaleSuperTextBox()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ParentRepoTheme {
-        Greeting("Android")
-    }
-}
